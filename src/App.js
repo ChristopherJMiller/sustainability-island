350ab6a2c7775f18d48d90beac402466d04118d6
Christopher Miller <admin@mctherealm.net>
2017-06-27 19:25:52 -0400

    Created generation table

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import GenerationTable from './components/generationtable';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: [
+        {
+          name: 'John Smith',
+          status: 'Employed',
+        },
+        {
+          name: 'Randal White',
+          status: 'Unemployed',
+        },
+        {
+          name: 'Stephanie Sanders',
+          status: 'Employed',
+        },
+        {
+          name: 'Steve Brown',
+          status: 'Employed',
+        },
+        {
+          name: 'Joyce Whitten',
+          status: 'Employed',
+        },
+        {
+          name: 'Samuel Roberts',
+          status: 'Employed',
+        },
+        {
+          name: 'Adam Moore',
+          status: 'Employed',
+        },
+      ]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        
+        <GenerationTable tableData={this.state.tableData} />
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/generationtable.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+import {
+  Table,
+  TableBody,
+  TableFooter,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+
+const styles = {
+  propContainer: {
+    width: 200,
+    overflow: 'hidden',
+    margin: '20px auto 0',
+  },
+  propToggleHeader: {
+    margin: '20px auto 10px',
+  },
+};
+
+/**
+ * A more complex example, allowing the table height to be set, and key boolean properties to be toggled.
+ */
+export default class GenerationTable extends Component {
+  state = {
+    fixedHeader: true,
+    fixedFooter: false,
+    stripedRows: false,
+    showRowHover: false,
+    selectable: false,
+    multiSelectable: false,
+    enableSelectAll: false,
+    deselectOnClickaway: true,
+    showCheckboxes: false
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleToggle = (event, toggled) => {
+    this.setState({
+      [event.target.name]: toggled,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({height: event.target.value});
+  };
+
+  render() {
+    return (
+      <div>
+        <Table
+          height={this.state.height}
+          fixedHeader={this.state.fixedHeader}
+          fixedFooter={this.state.fixedFooter}
+          selectable={this.state.selectable}
+          multiSelectable={this.state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={this.state.showCheckboxes}
+            adjustForCheckbox={this.state.showCheckboxes}
+            enableSelectAll={this.state.enableSelectAll}
+          >
+            <TableRow>
+              <TableHeaderColumn colSpan="16" style={{textAlign: 'center'}}>
+                Generation Table
+              </TableHeaderColumn>
+            </TableRow>
+            <TableRow>
+              <TableHeaderColumn tooltip="The Generation Number">Generation</TableHeaderColumn>
+              <TableHeaderColumn tooltip="The # of People at the start of Generation">People</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Type(s) of employment & # people it supports">Employment</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Type(s) of energy used & # people it supports">Energy Use</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Available Water">Water</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Available Land">Land</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Available Energy">Energy</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# of water before redemption">Water before redemp.</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# Discarded water after redemption">Discarded Water</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# of land before redemption">Land before redemp.</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# Discarded land after redemption">Discarded Land</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# of energy before redemption">Energy before redemp.</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# Discarded energy after redemption">Discarded Energy</TableHeaderColumn>
+              <TableHeaderColumn tooltip="(1.75x) Population increase (births & immigration)">Population Increase</TableHeaderColumn>
+              <TableHeaderColumn tooltip="- 20% death & emigration">Death & Emigration</TableHeaderColumn>
+              <TableHeaderColumn tooltip="# of People at the end of Generation">Pop.</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+          >
+            {this.props.tableData.map( (row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{index}</TableRowColumn>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.status}</TableRowColumn>
+              </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+    );
+  }
+}
